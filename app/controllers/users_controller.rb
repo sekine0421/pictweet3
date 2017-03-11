d604d51c42bd80e21a0077e394e14d029912934d
関根大輔 <sekinedaisuke@sekinedaisuke-no-MacBook-Pro.local>
2017-03-11 23:35:26 +0900

    ツイート一覧画面にニックネームを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,5 @@ class UsersController < ApplicationController
     @nickname = current_user.nickname
     @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
+  
 end
