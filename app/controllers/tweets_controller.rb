e50b1b0040fc0e99dc76b6320a1c28f9cd7f8506
関根大輔 <sekinedaisuke@sekinedaisuke-no-MacBook-Pro.local>
2017-03-13 19:35:09 +0900

    ツイートにコメント機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,7 +31,9 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
   end
+
 
   private
   def tweet_params
